b42b33a0b0fe8ede4b29d44115670563aa4a58aa
Elvismutinda <elvocool@gmail.com>
2023-08-28 16:09:18 +0300

    Working on user login page

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,11 +1,57 @@
-import React from 'react'
+"use client";
 
-type Props = {}
+import { cn } from "@/lib/utils";
+import { loginUserForm, loginUserSchema } from "@/lib/validations/user";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LuLoader2 } from "react-icons/lu";
+import { Button, Form, Input } from "antd";
 
-const UserAuthForm = (props: Props) => {
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const UserAuthForm = ({ className, ...props }: UserAuthFormProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<loginUserForm>({
+    resolver: zodResolver(loginUserSchema),
+  });
+
+  const onSubmit = async (data: loginUserForm) => {};
+
   return (
-    <div>UserAuthForm</div>
-  )
-}
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Form>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input {...register("email")} disabled={isSubmitting} />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password {...register("password")} disabled={isSubmitting} />
+          </Form.Item>
+          <Form.Item>
+            <Button disabled={isSubmitting} htmlType="submit">
+              {isSubmitting && (
+                <LuLoader2 className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </form>
+    </div>
+  );
+};
 
-export default UserAuthForm
+export default UserAuthForm;
