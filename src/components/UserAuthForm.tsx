b7a1c10847fad1f9c23c5c3b4fb07e70ae0dac72
Elvismutinda <elvocool@gmail.com>
2023-08-28 20:53:39 +0300

    Fixes for login form not submitting

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,56 +1,103 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import { loginUserForm, loginUserSchema } from "@/lib/validations/user";
-import React from "react";
-import { useForm } from "react-hook-form";
+import React, { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { LuLoader2 } from "react-icons/lu";
-import { Button, Form, Input } from "antd";
+import { Button, Input } from "antd";
+import { signIn, useSession } from "next-auth/react";
+import { toast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
-interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+const UserAuthForm = () => {
+  const router = useRouter();
 
-const UserAuthForm = ({ className, ...props }: UserAuthFormProps) => {
   const {
-    register,
+    control,
     handleSubmit,
-    formState: { errors, isSubmitting },
-    reset,
+    formState: { errors },
   } = useForm<loginUserForm>({
     resolver: zodResolver(loginUserSchema),
   });
 
-  const onSubmit = async (data: loginUserForm) => {};
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const onSubmit = async (data: loginUserForm) => {
+    setIsLoading(true);
+
+    const signInResult = await signIn("credentials", {
+      email: data.email.toLowerCase(),
+      password: data.password,
+    });
+
+    setIsLoading(false);
+
+    if (!signInResult?.ok) {
+      return toast({
+        title: "Something went wrong.",
+        description: "Your sign in request failed. Please try again later.",
+        variant: "destructive",
+      });
+    }
+
+    const { data: session } = await useSession();
+
+    // Check if the user is authenticated and has a role
+    if (session && session.user && session.user.role) {
+      switch (session.user.role) {
+        case "Admin":
+          router.push("/admin");
+          break;
+        case "Incharge":
+          router.push("/incharge");
+          break;
+        case "Police":
+          router.push("/police");
+          break;
+        case "Detective":
+          router.push("/detective");
+          break;
+        default:
+          // Handle other roles or scenarios
+          break;
+      }
+    }
+
+    return toast({
+      title: "Successfully logged in",
+    });
+  };
 
   return (
-    <div className={cn("grid gap-6", className)} {...props}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Form>
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[{ required: true, message: "Please input your email!" }]}
-          >
-            <Input {...register("email")} disabled={isSubmitting} />
-          </Form.Item>
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
-          >
-            <Input.Password {...register("password")} disabled={isSubmitting} />
-          </Form.Item>
-          <Form.Item>
-            <Button disabled={isSubmitting} htmlType="submit">
-              {isSubmitting && (
-                <LuLoader2 className="mr-2 h-4 w-4 animate-spin" />
-              )}
-              Login
-            </Button>
-          </Form.Item>
-        </Form>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="email"
+        control={control}
+        defaultValue=""
+        rules={{ required: true }}
+        render={({ field }) => (
+          <Input size="large" {...field} placeholder="Email Address" />
+        )}
+      />
+      <Controller
+        name="password"
+        control={control}
+        defaultValue=""
+        rules={{ required: true }}
+        render={({ field }) => (
+          <Input.Password
+            size="large"
+            {...field}
+            placeholder="*****************"
+          />
+        )}
+      />
+      <Button disabled={isLoading} htmlType="submit">
+        {isLoading && <LuLoader2 className="mr-2 h-4 w-4 animate-spin" />}
+        Login
+      </Button>
+    </form>
   );
 };
 
