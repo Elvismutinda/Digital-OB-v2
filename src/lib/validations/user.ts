4e873703eecad57e3bcc4b6955d2c14eb61b3da4
Elvismutinda <elvocool@gmail.com>
2023-08-28 16:07:18 +0300

    Added validations for user operations

--- src/lib/validations/user.ts
+++ src/lib/validations/user.ts
@@ -1,17 +1,21 @@
 import * as z from "zod";
 
 export const registerUserSchema = z.object({
+  staffId: z
+    .string({ required_error: "Staff ID is required" })
+    .min(1, "Staff ID is required"),
+    email: z
+      .string({ required_error: "Email is required" })
+      .min(1, "Email is required")
+      .email("Invalid email address"),
   name: z
     .string({ required_error: "Name is required" })
     .min(1, "Full name is required"),
-  email: z
-    .string({ required_error: "Email is required" })
-    .min(1, "Email is required")
-    .email("Invalid email address"),
-  password: z
-    .string({ required_error: "Password is required" })
-    .min(1, "Password is required")
-    .min(8, "Password must be at least 8 characters"),
+    password: z
+      .string({ required_error: "Password is required" })
+      .min(1, "Password is required")
+      .min(8, "Password must be at least 8 characters"),
+  role: z.string({ required_error: "Role is required" }),
 });
 
 export const loginUserSchema = z.object({
@@ -25,5 +29,23 @@ export const loginUserSchema = z.object({
     .min(8, "Password must be at least 8 characters"),
 });
 
-export type loginUserInput = z.infer<typeof loginUserSchema>;
-export type registerUserInput = z.infer<typeof registerUserSchema>;
+export const updateUserSchema = z
+  .object({
+    password: z
+      .string({ required_error: "Password is required" })
+      .min(1, "Password is required"),
+    newPassword: z
+      .string({ required_error: "New password is required" })
+      .min(1, "New password is required"),
+    confirmNewPassword: z
+      .string({ required_error: "Confirm your new password" })
+      .min(1, "Confirm your new password"),
+  })
+  .refine((data) => data.newPassword === data.confirmNewPassword, {
+    path: ["confirmNewPassword"],
+    message: "Passwords do not match",
+  });
+
+export type registerUserForm = z.infer<typeof registerUserSchema>;
+export type loginUserForm = z.infer<typeof loginUserSchema>;
+export type updateUserForm = z.infer<typeof updateUserSchema>;
