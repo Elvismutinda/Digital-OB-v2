b9b9c6f44542cb6d0f9e6238120669abe38ca70a
Elvismutinda <elvocool@gmail.com>
2023-08-29 12:09:15 +0300

    Updated prisma db configuration

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,22 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
-
 declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma: PrismaClient;
-    }
-  }
+  var prisma: PrismaClient | undefined;
 }
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+export const db = globalThis.prisma || new PrismaClient();
 
-export const db = prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
