b9f945e9f49565b80b055eabc06dbb96ac9747a4
Elvismutinda <elvocool@gmail.com>
2023-08-30 12:08:02 +0300

    Home page nav items

---
+++ src/config/details.ts
@@ -0,0 +1,18 @@
+import { DetailsConfig } from "@/types";
+
+export const detailsConfig: DetailsConfig = {
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Credentials",
+      href: "/#credentials",
+    },
+    {
+      title: "Features",
+      href: "/#features",
+    },
+  ],
+};
