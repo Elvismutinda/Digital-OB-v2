8df45975f968a7936894210800f796241fb54c67
Elvismutinda <elvocool@gmail.com>
2023-08-30 12:01:58 +0300

    constant variables

---
+++ src/types/index.d.ts
@@ -0,0 +1,21 @@
+export type SiteConfig = {
+  name: string;
+  description: string;
+  url: string;
+  links: {
+    twitter: string;
+    github: string;
+  };
+};
+
+export type NavItem = {
+  title: string;
+  href: string;
+  disabled?: boolean;
+};
+
+export type MainNavItem = NavItem;
+
+export type DetailsConfig = {
+  mainNav: MainNavItem[];
+};
