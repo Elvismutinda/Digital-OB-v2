731e0c923726649711c14f200ce530bafe08414b
Elvismutinda <elvocool@gmail.com>
2023-08-30 12:08:33 +0300

    Site details

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Digital O.B",
+  description: "Digital version of the police O.B",
+  url: "https://digitalob.vercel.app",
+  links: {
+    twitter: "https://twitter.com/Elvocool",
+    github: "https://github.com/Elvismutinda",
+  },
+};
