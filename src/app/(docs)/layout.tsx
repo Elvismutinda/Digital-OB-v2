aad9f9ee19034508a9aa83894ee30ba1e8f1428d
Elvismutinda <elvocool@gmail.com>
2023-08-30 21:24:30 +0300

    Docs layout

---
+++ src/app/(docs)/layout.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Link from "next/link";
+
+import { docsConfig } from "@/config/docs";
+import { siteConfig } from "@/config/site";
+import { MainNav } from "@/components/MainNav";
+import { DocsSearch } from "@/components/DocsSearch";
+import { DocsSidebarNav } from "@/components/DocsSidebarNav";
+import SiteFooter from "@/components/SiteFooter";
+import { FaGithub } from "react-icons/fa";
+
+export default function DocsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <header className="sticky top-0 z-40 w-full border-b bg-background">
+        <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+          <MainNav items={docsConfig.mainNav}>
+            <DocsSidebarNav items={docsConfig.sidebarNav} />
+          </MainNav>
+          <div className="flex flex-1 items-center space-x-4 sm:justify-end">
+            <div className="flex-1 sm:grow-0">
+              <DocsSearch />
+            </div>
+            <nav className="flex space-x-4">
+              <Link
+                href={siteConfig.links.github}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaGithub className="h-7 w-7" />
+                <span className="sr-only">GitHub</span>
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </header>
+      <div className="container flex-1">{children}</div>
+      <SiteFooter className="border-t" />
+    </div>
+  );
+}
