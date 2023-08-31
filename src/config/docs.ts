1b0ed7b098ff107976d68d96bea469b5bc0fde06
Elvismutinda <elvocool@gmail.com>
2023-08-31 12:33:41 +0300

    Added my project report headers

--- src/config/docs.ts
+++ src/config/docs.ts
@@ -1,136 +1,149 @@
-import { DocsConfig } from "@/types"
+import { DocsConfig } from "@/types";
 
 export const docsConfig: DocsConfig = {
-    mainNav: [
-      {
-        title: "Documentation",
-        href: "/docs",
-      },
-      {
-        title: "Guides",
-        href: "/guides",
-      },
-    ],
-    sidebarNav: [
-      {
-        title: "Getting Started",
-        items: [
-          {
-            title: "Introduction",
-            href: "/docs",
-          },
-        ],
-      },
-      {
-        title: "Documentation",
-        items: [
-          {
-            title: "Introduction",
-            href: "/docs/documentation",
-          },
-          {
-            title: "Contentlayer",
-            href: "/docs/in-progress",
-            disabled: true,
-          },
-          {
-            title: "Components",
-            href: "/docs/documentation/components",
-          },
-          {
-            title: "Code Blocks",
-            href: "/docs/documentation/code-blocks",
-          },
-          {
-            title: "Style Guide",
-            href: "/docs/documentation/style-guide",
-          },
-          {
-            title: "Search",
-            href: "/docs/in-progress",
-            disabled: true,
-          },
-        ],
-      },
-      {
-        title: "Blog",
-        items: [
-          {
-            title: "Introduction",
-            href: "/docs/in-progress",
-            disabled: true,
-          },
-          {
-            title: "Build your own",
-            href: "/docs/in-progress",
-            disabled: true,
-          },
-          {
-            title: "Writing Posts",
-            href: "/docs/in-progress",
-            disabled: true,
-          },
-        ],
-      },
-      {
-        title: "Dashboard",
-        items: [
-          {
-            title: "Introduction",
-            href: "/docs/in-progress",
-            disabled: true,
-          },
-          {
-            title: "Layouts",
-            href: "/docs/in-progress",
-            disabled: true,
-          },
-          {
-            title: "Server Components",
-            href: "/docs/in-progress",
-            disabled: true,
-          },
-          {
-            title: "Authentication",
-            href: "/docs/in-progress",
-            disabled: true,
-          },
-          {
-            title: "Database with Prisma",
-            href: "/docs/in-progress",
-            disabled: true,
-          },
-          {
-            title: "API Routes",
-            href: "/docs/in-progress",
-            disabled: true,
-          },
-        ],
-      },
-      {
-        title: "Marketing Site",
-        items: [
-          {
-            title: "Introduction",
-            href: "/docs/in-progress",
-            disabled: true,
-          },
-          {
-            title: "File Structure",
-            href: "/docs/in-progress",
-            disabled: true,
-          },
-          {
-            title: "Tailwind CSS",
-            href: "/docs/in-progress",
-            disabled: true,
-          },
-          {
-            title: "Typography",
-            href: "/docs/in-progress",
-            disabled: true,
-          },
-        ],
-      },
-    ],
-  }
+  mainNav: [
+    {
+      title: "Documentation",
+      href: "/docs",
+    },
+    {
+      title: "Login from Home",
+      href: "/",
+    },
+  ],
+  sidebarNav: [
+    {
+      title: "Project Documentation",
+      items: [
+        {
+          title: "Overview",
+          href: "/docs",
+        },
+      ],
+    },
+    {
+      title: "INTRODUCTION",
+      items: [
+        {
+          title: "Background of the Study",
+          href: "/docs/documentation",
+        },
+        {
+          title: "Problem Statement",
+          href: "/docs/in-progress",
+          disabled: true,
+        },
+        {
+          title: "Objectives",
+          href: "/docs/documentation/components",
+        },
+        {
+          title: "Scope of the Study",
+          href: "/docs/documentation/code-blocks",
+        },
+      ],
+    },
+    {
+      title: "LITERATURE REVIEW",
+      items: [
+        {
+          title: "Definition of Crime",
+          href: "/docs/in-progress",
+          disabled: true,
+        },
+        {
+          title: "Classification of Crime",
+          href: "/docs/in-progress",
+          disabled: true,
+        },
+        {
+          title: "Crime in Kenya",
+          href: "/docs/in-progress",
+          disabled: true,
+        },
+        {
+          title: "Proposed Solution",
+          href: "/docs/in-progress",
+        },
+        {
+          title: "Existing Crime Management Systems",
+          href: "/docs/in-progress",
+        },
+      ],
+    },
+    {
+      title: "SYSTEM ANALYSIS & DESIGN",
+      items: [
+        {
+          title: "System Development Methodology",
+          href: "/docs/in-progress",
+          disabled: true,
+        },
+        {
+          title: "Requirements Elicitation",
+          href: "/docs/in-progress",
+          disabled: true,
+        },
+        {
+          title: "System Design",
+          href: "/docs/in-progress",
+          disabled: true,
+        },
+      ],
+    },
+    {
+      title: "SYSTEM IMPLEMENTATION",
+      items: [
+        {
+          title: "Technologies Used",
+          href: "/docs/in-progress",
+          disabled: true,
+        },
+        {
+          title: "System Testing",
+          href: "/docs/in-progress",
+          disabled: true,
+        },
+        {
+          title: "Test Cases and Results",
+          href: "/docs/in-progress",
+          disabled: true,
+        },
+      ],
+    },
+    {
+      title: "CONCLUSION",
+      items: [
+        {
+          title: "Recommendations",
+          href: "/docs/in-progress",
+        },
+        {
+          title: "References",
+          href: "/docs/in-progress",
+        },
+      ],
+    },
+    {
+      title: "APPENDICES",
+      items: [
+        {
+          title: "User Manual",
+          href: "/docs/in-progress",
+        },
+        {
+          title: "Sample Code",
+          href: "/docs/in-progress",
+        },
+        {
+          title: "Work Plan",
+          href: "/docs/in-progress",
+        },
+        {
+          title: "Interview Guide",
+          href: "/docs/in-progress",
+        },
+      ],
+    },
+  ],
+};
