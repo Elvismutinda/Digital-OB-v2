de74bbde8ea708c0b62dc949329a240b15690ba6
Elvismutinda <elvocool@gmail.com>
2023-09-01 16:17:22 +0300

    Stations route

--- src/app/(admin)/admin/stations/loading.tsx
+++ src/app/(admin)/admin/stations/loading.tsx
@@ -1,16 +1,13 @@
 import CardSkeleton from "@/components/CardSkeleton";
 import SiteHeader from "@/components/SiteHeader";
 import SiteShell from "@/components/SiteShell";
-import { Button, buttonVariants } from "@/components/ui/Button";
-import { cn } from "@/lib/utils";
+import StationCreateButton from "@/components/admin/StationCreateButton";
 
 const AdminStationLoading = () => {
   return (
     <SiteShell>
       <SiteHeader heading="Stations">
-        <Button className={cn(buttonVariants({ variant: "default" }))}>
-          Test
-        </Button>
+        <StationCreateButton />
       </SiteHeader>
       <div className="grid gap-10">
         <CardSkeleton />
