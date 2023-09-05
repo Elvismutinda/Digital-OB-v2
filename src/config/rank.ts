ca41b628f6adc81f77cd6efa1eeaabcdcc1f7743
Elvismutinda <elvocool@gmail.com>
2023-09-05 13:52:25 +0300

    Added officer ranks

---
+++ src/config/rank.ts
@@ -0,0 +1,42 @@
+export const ranksData = [
+  {
+    id: 1,
+    name: "Chief Inspector",
+  },
+  {
+    id: 2,
+    name: "Assistant Superintendent",
+  },
+  {
+    id: 3,
+    name: "Senior Inspector",
+  },
+  {
+    id: 4,
+    name: "Inspector",
+  },
+  {
+    id: 5,
+    name: "Assistant Inspector",
+  },
+  {
+    id: 6,
+    name: "Sergeant Major",
+  },
+  {
+    id: 7,
+    name: "Sergeant",
+  },
+  {
+    id: 8,
+    name: "Corporal",
+  },
+  {
+    id: 9,
+    name: "Lance Corporal",
+  },
+  {
+    id: 10,
+    name: "Constable",
+  },
+] as const;
