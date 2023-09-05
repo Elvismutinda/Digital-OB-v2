5e115bca9a49cb7d8a157b5eda9c57ad32c7ea3e
Elvismutinda <elvocool@gmail.com>
2023-09-05 13:53:05 +0300

    Added station validation

---
+++ src/lib/validations/station.ts
@@ -0,0 +1,13 @@
+import * as z from "zod";
+
+export const registerStationSchema = z.object({
+  name: z
+    .string({ required_error: "Station name is required" })
+    .min(1, "Station name is required"),
+  county: z.string({ required_error: "County is required" }),
+  sub_county: z.string({ required_error: "Sub-county is required" }),
+  contact: z
+    .string({ required_error: "Contact is required" })
+    .min(1, "Contact is required")
+    .max(13, "Enter a valid phone number"),
+});
