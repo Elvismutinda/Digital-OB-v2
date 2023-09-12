09aeee820efd06d868399423b94966d53eab1939
Elvismutinda <elvocool@gmail.com>
2023-09-12 14:51:35 +0300

    Admin dashboard component

---
+++ src/components/admin/DashboardAdmin.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/Tabs";
+import Overview from "./Overview";
+import Analytics from "./Analytics";
+import Reports from "./Reports";
+
+type Props = {};
+
+const DashboardAdmin = (props: Props) => {
+  return (
+    <div className="flex-1 space-y-4 py-8 pt-6">
+      <Tabs defaultValue="overview" className="space-y-4">
+        <TabsList>
+          <TabsTrigger value="overview">Overview</TabsTrigger>
+          <TabsTrigger value="analytics">Analytics</TabsTrigger>
+          <TabsTrigger value="reports">Reports</TabsTrigger>
+        </TabsList>
+        <TabsContent value="overview" className="space-y-4">
+          <Overview />
+        </TabsContent>
+        <TabsContent value="analytics" className="space-y-4">
+          <Analytics />
+        </TabsContent>
+        <TabsContent value="reports" className="space-y-4">
+          <Reports />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default DashboardAdmin;
