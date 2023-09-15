8b82202477b4808e9f58395a19d3d83e6bdf486f
Elvismutinda <elvocool@gmail.com>
2023-09-15 15:31:09 +0300

    Adding api delete and patch requests for the table data

--- src/components/admin/StaffDetails.tsx
+++ src/components/admin/StaffDetails.tsx
@@ -4,61 +4,10 @@ import { Button, Popconfirm, Space, Table } from "antd";
 import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 import React, { useEffect } from "react";
 import axios from "axios";
-
-const cols = [
-  {
-    title: "Staff ID",
-    dataIndex: "staffId",
-    key: "staffId",
-  },
-  {
-    title: "Full Name",
-    dataIndex: "name",
-    key: "name",
-  },
-  {
-    title: "Rank",
-    dataIndex: "rank",
-    key: "rank",
-  },
-  {
-    title: "Email",
-    dataIndex: "email",
-    key: "email",
-  },
-  {
-    title: "Station",
-    dataIndex: "station",
-    key: "station",
-  },
-  {
-    title: "Role",
-    dataIndex: "role",
-    key: "role",
-  },
-  {
-    title: "Gender",
-    dataIndex: "gender",
-    key: "gender",
-  },
-  {
-    title: "Action",
-    key: "action",
-    render: (record) => (
-      <Space direction="horizontal">
-        <Button icon={<EditOutlined />}></Button>
-        <Popconfirm
-          title="Are you sure you want to delete this staff?"
-          okText="Yes"
-        >
-          <Button icon={<DeleteOutlined />} />
-        </Popconfirm>
-      </Space>
-    ),
-  },
-];
+import { toast } from "@/hooks/use-toast";
 
 interface StaffData {
+  id: string;
   staffId: string;
   name: string;
   rank: string;
@@ -82,7 +31,7 @@ const StaffDetails = () => {
     // console.log(res.data)
     setIsLoading(false);
     setData(
-      res.data.map((row) => ({
+      res.data.map((row: StaffData) => ({
         staffId: row.staffId,
         name: row.name,
         rank: row.rank,
@@ -90,17 +39,99 @@ const StaffDetails = () => {
         station: row.station,
         role: row.role,
         gender: row.gender,
-        key: row.staffId,
+        key: row.id,
       }))
     );
   };
+
+  async function handleDelete(userId: string) {
+    const res = await axios.delete(`/api/users/${userId}`);
+
+    if (res.status === 404) {
+      toast({
+        title: "Error",
+        description: "Staff not found",
+        variant: "destructive",
+      });
+    }
+
+    if (res.status === 200) {
+      toast({
+        title: "Success",
+        description: "Staff deleted successfully",
+      });
+
+      getData();
+    }
+
+    if (res.status === 500) {
+      toast({
+        title: "Error",
+        description: "Something went wrong. Try again later!",
+        variant: "destructive",
+      });
+    }
+  }
+
   return (
     <div className="max-w-full">
       <div>
         <Table
           size="small"
           loading={isLoading}
-          columns={cols}
+          columns={[
+            {
+              title: "Staff ID",
+              dataIndex: "staffId",
+              key: "staffId",
+            },
+            {
+              title: "Full Name",
+              dataIndex: "name",
+              key: "name",
+            },
+            {
+              title: "Rank",
+              dataIndex: "rank",
+              key: "rank",
+            },
+            {
+              title: "Email",
+              dataIndex: "email",
+              key: "email",
+            },
+            {
+              title: "Station",
+              dataIndex: "station",
+              key: "station",
+            },
+            {
+              title: "Role",
+              dataIndex: "role",
+              key: "role",
+            },
+            {
+              title: "Gender",
+              dataIndex: "gender",
+              key: "gender",
+            },
+            {
+              title: "Action",
+              key: "action",
+              render: (record) => (
+                <Space direction="horizontal">
+                  <Button icon={<EditOutlined />}></Button>
+                  <Popconfirm
+                    title="Are you sure you want to delete this staff?"
+                    okText="Yes"
+                    onConfirm={() => handleDelete(record.key)}
+                  >
+                    <Button icon={<DeleteOutlined />} />
+                  </Popconfirm>
+                </Space>
+              ),
+            },
+          ]}
           dataSource={data}
           pagination={{
             defaultPageSize: 15,
