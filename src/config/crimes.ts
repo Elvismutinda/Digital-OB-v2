80b74f415797615c3c7ff6b58270807b7d93b766
Elvismutinda <elvocool@gmail.com>
2023-09-15 15:35:49 +0300

    Added crimes

---
+++ src/config/crimes.ts
@@ -0,0 +1,82 @@
+export const crimeData = [
+  {
+    id: 1,
+    type: "Assault",
+  },
+  {
+    id: 2,
+    type: "Burglary",
+  },
+  {
+    id: 3,
+    type: "Child Abuse",
+  },
+  {
+    id: 4,
+    type: "Child Labour",
+  },
+  {
+    id: 5,
+    type: "Child Trafficking",
+  },
+  {
+    id: 6,
+    type: "Corruption",
+  },
+  {
+    id: 7,
+    type: "Cyber Crime",
+  },
+  {
+    id: 8,
+    type: "Domestic Violence",
+  },
+  {
+    id: 9,
+    type: "Drug Abuse",
+  },
+  {
+    id: 10,
+    type: "Extortion",
+  },
+  {
+    id: 11,
+    type: "Femicide",
+  },
+  {
+    id: 12,
+    type: "Forgery",
+  },
+  {
+    id: 13,
+    type: "Fraud",
+  },
+  {
+    id: 14,
+    type: "Road Accident",
+  },
+  {
+    id: 15,
+    type: "Defilement",
+  },
+  {
+    id: 16,
+    type: "Murder",
+  },
+  {
+    id: 17,
+    type: "Rape",
+  },
+  {
+    id: 18,
+    type: "Robbery",
+  },
+  {
+    id: 19,
+    type: "Vandalism",
+  },
+  {
+    id: 20,
+    type: "Other",
+  },
+];
