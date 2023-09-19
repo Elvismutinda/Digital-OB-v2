985a30dca0efd466ff7558183998a65b96046abc
Elvismutinda <elvocool@gmail.com>
2023-09-19 22:32:22 +0300

    PATCH request added

--- src/app/api/users/[userId]/route.ts
+++ src/app/api/users/[userId]/route.ts
@@ -1,5 +1,6 @@
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { userDetailsSchema } from "@/lib/validations/user";
 import { z } from "zod";
 
 const routeContextSchema = z.object({
@@ -18,12 +19,6 @@ export async function DELETE(
     if (session?.user.role !== "Admin") {
       return new Response("Unauthorized", { status: 401 });
     }
-    // Parse the user ID from the request parameters
-    // const params = z.object({
-    //   userId: z.string(),
-    // });
-
-    // const { userId } = params.parse(req.params);
 
     const { params } = routeContextSchema.parse(context);
 
@@ -48,5 +43,47 @@ export async function DELETE(
     return new Response("User deleted successfully", { status: 200 });
   } catch (error) {
     return new Response("Could not delete user", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  context: z.infer<typeof routeContextSchema>
+) {
+  try {
+    const session = await getAuthSession();
+
+    if (session?.user.role !== "Admin") {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const { params } = routeContextSchema.parse(context);
+
+    const json = await req.json();
+    const body = userDetailsSchema.parse(json);
+
+    // update the user
+    await db.user.update({
+      where: {
+        id: params.userId,
+      },
+      data: {
+        name: body.name,
+        email: body.email,
+        rank: body.rank,
+        role: body.role,
+        gender: body.gender,
+      },
+    });
+
+    return new Response("User updated successfully", { status: 200 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid PATCH request data passed", {
+        status: 422,
+      });
+    }
+
+    return new Response("Could not update station", { status: 500 });
   }
 }
