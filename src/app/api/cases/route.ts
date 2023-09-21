83a19221dfb204fe87027eaebef464958845457b
Elvismutinda <elvocool@gmail.com>
2023-09-21 14:54:51 +0300

    POST request for cases

---
+++ src/app/api/cases/route.ts
@@ -0,0 +1,60 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { registerCaseSchema } from "@/lib/validations/case";
+import { z } from "zod";
+
+export async function POST(req: Request) {
+  try {
+    const session = await getAuthSession();
+
+    if (session?.user.role !== "Police") {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const body = await req.json();
+    const { ob_number, crime, statement, complainantId } =
+      registerCaseSchema.parse(body);
+
+    // check if case already exists
+    const caseExists = await db.case.findFirst({
+      where: {
+        ob_number,
+      },
+    });
+
+    if (caseExists) {
+      return new Response("Case already exists", { status: 409 });
+    }
+
+    // create case and associate with complainant, station and user
+    const user = await db.user.findFirst({
+      select: {
+        stationId: true,
+      },
+      where: {
+        id: session.user.id,
+      },
+    });
+
+    const stationId = user?.stationId ?? "";
+
+    const createCase = await db.case.create({
+      data: {
+        ob_number,
+        crime,
+        statement,
+        creatorId: session.user.id,
+        complainantId,
+        stationId,
+      },
+    });
+
+    return new Response("Case Created", { status: 200 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid POST request data passed", { status: 422 });
+    }
+
+    return new Response(error, { status: 500 });
+  }
+}
