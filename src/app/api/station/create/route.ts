16b1cc512fbb432a2b5d0524745fc4f4a976cc99
Elvismutinda <elvocool@gmail.com>
2023-10-02 13:52:41 +0300

    POST request for creating stations

---
+++ src/app/api/station/create/route.ts
@@ -0,0 +1,48 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { stationSchema } from "@/lib/validations/station";
+import { z } from "zod";
+
+export async function POST(req: Request) {
+  try {
+    const session = await getAuthSession();
+
+    if (session?.user.role !== "Admin") {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const body = await req.json();
+    const { name, county, sub_county, contact } =
+      stationSchema.parse(body);
+
+    // check if station already exists
+    const stationExists = await db.station.findFirst({
+      where: {
+        name,
+      },
+    });
+
+    if (stationExists) {
+      return new Response("Station already exists", { status: 409 });
+    }
+
+    // create station
+
+    const station = await db.station.create({
+      data: {
+        name,
+        county,
+        sub_county,
+        contact,
+      },
+    });
+
+    return new Response(station.name, { status: 201 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid POST request data passed", { status: 422 });
+    }
+
+    return new Response("Could not create station", { status: 500 });
+  }
+}
